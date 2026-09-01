0ad1236a7ad302be0ec3f92dce1e1b20aed58394
MayadaALMaghribi <latifa293h@gmail.com>
2026-09-02 01:54:25 +0300

    create bottom navigation bar design

--- lib/feature/home/home_screen.dart
+++ lib/feature/home/home_screen.dart
@@ -1,10 +1,57 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:islami/core/utils/app_assets.dart';
+import 'package:islami/core/utils/app_colors.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(bottomNavigationBar: buidldBottomNavigationBar());
+  }
+
+  buidldBottomNavigationBar() {
+    return BottomNavigationBar(
+      selectedItemColor: AppColors.white,
+
+      unselectedItemColor: AppColors.black,
+
+      items: [
+        buildBottomNavigationBarItem(image: AppAssets.icQuran, label: "Quran"),
+        buildBottomNavigationBarItem(
+          image: AppAssets.icHadeth,
+          label: "Hadeth",
+        ),
+        buildBottomNavigationBarItem(image: AppAssets.icSebha, label: "Sebha"),
+        buildBottomNavigationBarItem(image: AppAssets.icRadio, label: "Radio"),
+
+        buildBottomNavigationBarItem(image: AppAssets.icTime, label: "Time"),
+      ],
+    );
+  }
+
+  BottomNavigationBarItem buildBottomNavigationBarItem({
+    required String image,
+    required String label,
+  }) {
+    return BottomNavigationBarItem(
+      icon: Container(
+        padding: EdgeInsets.symmetric(vertical: 6, horizontal: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(66),
+          color: AppColors.black.withAlpha(153),
+        ),
+
+        child: ImageIcon(AssetImage(image)),
+      ),
+      label: label,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:islami/core/utils/app_colors.dart';
+import 'package:islami/feature/home/home_screen.dart';
 import 'package:islami/feature/splash/splash_screen.dart';
 
 void main() {
@@ -10,6 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    return MaterialApp(
+      theme: ThemeData(canvasColor: AppColors.gold),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
