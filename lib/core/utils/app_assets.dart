b07f3d5c9c7aacc26d401a76b6b2694a8e6a2732
MayadaALMaghribi <latifa293h@gmail.com>
2026-09-02 00:35:31 +0300

    splash screen are done

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,6 +1,5 @@
 abstract final class AppAssets {
-
-  static const String _imagesPath = 'assets/images';
+  static const String _imagesPath = 'asset/images';
 
   static const String icHadeth = '$_imagesPath/ic_hadeth.png';
   static const String icQuran = '$_imagesPath/ic_quran.png';
@@ -9,8 +8,10 @@ abstract final class AppAssets {
   static const String icTime = '$_imagesPath/ic_time.png';
   static const String imgHadethBg = '$_imagesPath/img_hadeth_bg.png';
   static const String imgMostRecent = '$_imagesPath/img_most_recent.png';
-  static const String imgQuranBackground = '$_imagesPath/img_quran_background.png';
+  static const String imgQuranBackground =
+      '$_imagesPath/img_quran_background.png';
   static const String imgSplash = '$_imagesPath/img_splash.png';
-  static const String imgSuraNumberFrame = '$_imagesPath/img_sura_number_frame.png';
+  static const String imgSuraNumberFrame =
+      '$_imagesPath/img_sura_number_frame.png';
   static const String islamiLogo = '$_imagesPath/islami_logo.png';
 }
