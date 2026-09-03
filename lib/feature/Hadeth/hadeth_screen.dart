7729a633a147685c55d58213540ead603055bcd1
MayadaALMaghribi <latifa293h@gmail.com>
2026-09-04 01:52:22 +0300

    done

--- lib/feature/Hadeth/hadeth_screen.dart
+++ lib/feature/Hadeth/hadeth_screen.dart
@@ -1,28 +1,10 @@
-import 'dart:developer';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:islami/core/utils/app_assets.dart';
-import 'package:islami/feature/Hadeth/model/hadeth_data_model.dart';
 import 'package:islami/feature/Hadeth/widget/hadeth_widget.dart';
 
-class HadethScreen extends StatefulWidget {
+class HadethScreen extends StatelessWidget {
   const HadethScreen({super.key});
-
-  @override
-  State<HadethScreen> createState() => _HadethScreenState();
-}
-
-class _HadethScreenState extends State<HadethScreen> {
-  List<HadethDataModel> hadeeths = [];
-  String content = "";
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    readHadethFile();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,28 +27,83 @@ class _HadethScreenState extends State<HadethScreen> {
   }
 
   buildHadethCarousel() => CarouselSlider.builder(
-    itemCount: hadeeths.length,
+    itemCount: 50,
     itemBuilder: (context, index, realindex) {
-      return HadethWidget(
-        hadethDataModel: HadethDataModel(
-          title: hadeeths[index].title,
-          hadeth: hadeeths[index].hadeth,
-        ),
-      );
+      return HadethWidget(index: index);
     },
     options: CarouselOptions(enlargeCenterPage: true, height: double.infinity),
   );
-  readHadethFile() async {
-    List<String> lines;
-    for (int i = 1; i <= 50; i++) {
-      String fileName = "asset/files/hadeeth/h${i}.txt";
-      content = await rootBundle.loadString(fileName);
-      lines = content.split("\n");
-      hadeeths.add(
-        HadethDataModel(title: lines[0], hadeth: lines.sublist(1).join("\n")),
-      );
-    }
-    print("Hadtheds:$hadeeths");
-    setState(() {});
-  }
 }
+
+// import 'dart:developer';
+// import 'package:carousel_slider/carousel_slider.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:islami/core/utils/app_assets.dart';
+// import 'package:islami/feature/Hadeth/model/hadeth_data_model.dart';
+// import 'package:islami/feature/Hadeth/widget/hadeth_widget.dart';
+
+// class HadethScreen extends StatefulWidget {
+//   const HadethScreen({super.key});
+
+//   @override
+//   State<HadethScreen> createState() => _HadethScreenState();
+// }
+
+// class _HadethScreenState extends State<HadethScreen> {
+//   List<HadethDataModel> hadeeths = [];
+//   String content = "";
+
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//     readHadethFile();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         color: Colors.red,
+//         image: DecorationImage(
+//           image: AssetImage(AppAssets.imgHadethBg),
+//           fit: BoxFit.fill,
+//         ),
+//       ),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.stretch,
+//         children: [
+//           Image.asset(AppAssets.islamiLogo),
+//           Expanded(child: buildHadethCarousel()),
+//         ],
+//       ),
+//     );
+//   }
+
+//   buildHadethCarousel() => CarouselSlider.builder(
+//     itemCount: hadeeths.length,
+//     itemBuilder: (context, index, realindex) {
+//       return HadethWidget(
+//         hadethDataModel: HadethDataModel(
+//           title: hadeeths[index].title,
+//           hadeth: hadeeths[index].hadeth,
+//         ),
+//       );
+//     },
+//     options: CarouselOptions(enlargeCenterPage: true, height: double.infinity),
+//   );
+//   readHadethFile() async {
+//     List<String> lines;
+//     for (int i = 1; i <= 50; i++) {
+//       String fileName = "asset/files/hadeeth/h${i}.txt";
+//       content = await rootBundle.loadString(fileName);
+//       lines = content.split("\n");
+//       hadeeths.add(
+//         HadethDataModel(title: lines[0], hadeth: lines.sublist(1).join("\n")),
+//       );
+//     }
+//     print("Hadtheds:$hadeeths");
+//     setState(() {});
+//   }
+// }
--- lib/feature/Hadeth/widget/hadeth_widget.dart
+++ lib/feature/Hadeth/widget/hadeth_widget.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:islami/core/utils/app_assets.dart';
@@ -6,12 +5,24 @@ import 'package:islami/core/utils/app_colors.dart';
 import 'package:islami/core/utils/app_text_styles.dart';
 import 'package:islami/feature/Hadeth/model/hadeth_data_model.dart';
 
-class HadethWidget extends StatelessWidget {
-  const HadethWidget({required this.hadethDataModel, super.key});
-
-  final HadethDataModel hadethDataModel;
+class HadethWidget extends StatefulWidget {
+  const HadethWidget({required this.index, super.key});
+  final int index;
 
   @override
+  State<HadethWidget> createState() => _HadethWidgetState();
+}
+
+class _HadethWidgetState extends State<HadethWidget> {
+  String content = "";
+  HadethDataModel hadeeths = HadethDataModel(title: "", hadeth: "");
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readHadethFile();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -34,13 +45,13 @@ class HadethWidget extends StatelessWidget {
                   Image.asset(AppAssets.imgRightCorner, color: AppColors.black),
                 ],
               ),
-              Text(hadethDataModel.title, style: AppTextStyles.black24Bold),
+              Text(hadeeths.title, style: AppTextStyles.black24Bold),
             ],
           ),
           Expanded(
             child: SingleChildScrollView(
               child: Text(
-                hadethDataModel.hadeth,
+                hadeeths.hadeth,
                 style: AppTextStyles.black16Bold,
                 textAlign: TextAlign.center,
                 textDirection: TextDirection.rtl,
@@ -55,5 +66,19 @@ class HadethWidget extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  readHadethFile() async {
+    List<String> lines;
+    String fileName = "asset/files/hadeeth/h${widget.index + 1}.txt";
+    content = await rootBundle.loadString(fileName);
+    lines = content.split("\n");
+    hadeeths = HadethDataModel(
+      title: lines[0],
+      hadeth: lines.sublist(1).join("\n"),
+    );
+    print(hadeeths.hadeth);
+    print(hadeeths.title);
+    setState(() {});
   }
 }
